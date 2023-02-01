e9f5953fa96c992998d1e38e0ece080b296cbb9c
Prathik Shetty <prathikkshetty15@gmail.com>
2023-02-01 18:16:19 +0530

    craeted own nodejs api

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs')
+app.get('/', (req, res) => {
+  res.end('Hello World!');
+});
+app.get("/list_jokes", (req, res) => {
+    fs.readFile(__dirname + '/' + 'jokes.json', (err, data) => {
+        res.end(data);
+    });
+});
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+  });
